dd11209d8100c2c2d843c714142e00b2f1f5a78f
saganorrman <saga.a.c.norrman@gmail.com>
2025-05-09 18:10:29 +0200

    Skapade stack-modulen

---
+++ src/stack.js
@@ -0,0 +1,20 @@
+const _ = require('underscore');
+
+let stack = [];
+
+// Lägger ett element överst i stacken
+exports.push = function (x) {
+    stack.push(x);
+};
+
+// Returnerar det översta elementet i stacken och tar bort det
+exports.pop = function () {
+    return stack.pop();
+}
+
+// Returnerar det översta elementet i stacken
+exports.peek = function () {
+    return stack[0]; // Det här är medvetet felaktigt
+}
+
+//Den här funktionen returnerar det översta elementet i stacken utan att ta bort det. OBS! Här är en medveten bugg: den använder stack[0] istället för stack[stack.length - 1]. Detta ska du rätta senare när du skriver tester för denna funktion.
