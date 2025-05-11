9bd2450d4a6990974f6ffbd46c1eaf66c06ab422
saganorrman <saga.a.c.norrman@gmail.com>
2025-05-11 13:16:09 +0200

    byta till firefox

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,31 +1,28 @@
 const { Builder, By, until } = require('selenium-webdriver');
-require('chromedriver');
+require('geckodriver');
 
 const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
 const defaultTimeout = 10000;
 let driver;
-
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
-// Säkerställer att Chrome är igång
+// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
 beforeAll(async () => {
-    console.log(fileUnderTest);
-    driver = await new Builder().forBrowser('chrome').build();
+console.log(fileUnderTest);
+    driver = await new Builder().forBrowser('firefox').build();
     await driver.get(fileUnderTest);
 });
 
-// Avslutar Chrome efter testerna
-afterAll(async () => {
+// Allra sist avslutar vi Firefox igen
+afterAll(async() => {
     await driver.quit();
 }, defaultTimeout);
 
-//Test 1: Kontrollerar att stacken är tom i början
 test('The stack should be empty in the beginning', async () => {
     let stack = await driver.findElement(By.id('top_of_stack')).getText();
     expect(stack).toEqual("n/a");
 });
 
-// Test 2: Klicka på knappen och testa prompt
 describe('Clicking "Pusha till stacken"', () => {
     it('should open a prompt box', async () => {
         let push = await driver.findElement(By.id('push'));
