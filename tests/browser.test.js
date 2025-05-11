e4763d89703b225fedec79e5268ac24497be088e
saganorrman <saga.a.c.norrman@gmail.com>
2025-05-11 13:57:43 +0200

    Eget Selenium-test som misslyckas

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -4,6 +4,7 @@ require('geckodriver');
 const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
 const defaultTimeout = 10000;
 let driver;
+
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
 
 // Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
@@ -18,6 +19,10 @@ afterAll(async() => {
     await driver.quit();
 }, defaultTimeout);
 
+beforeEach(async () => {
+    await driver.get(fileUnderTest);
+});
+
 test('The stack should be empty in the beginning', async () => {
     let stack = await driver.findElement(By.id('top_of_stack')).getText();
     expect(stack).toEqual("n/a");
@@ -30,5 +35,24 @@ describe('Clicking "Pusha till stacken"', () => {
         let alert = await driver.switchTo().alert();
         await alert.sendKeys("Bananer");
         await alert.accept();
+    });
+});
+
+// EGET TEST – testa pop från tom stack
+describe('Clicking "Poppa från stacken" when stack is empty', () => {
+    it('should keep the top of stack as "n/a"', async () => {
+        let pop = await driver.findElement(By.id('pop'));
+        await pop.click();
+
+        // Fånga upp och stäng eventuell alert
+        try {
+            let alert = await driver.switchTo().alert();
+            await alert.dismiss(); // eller accept(), beroende på vad din JS gör
+        } catch (e) {
+            // Ingen alert? Ignorera
+        }
+
+        let stack = await driver.findElement(By.id('top_of_stack')).getText();
+        expect(stack).toEqual("n/a...");
     });
 });
